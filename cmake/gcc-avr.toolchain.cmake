b3ea4a46afa6308fba3ea7186dcde97be43c9283
patrik <patrik.wall.91@gmail.com>
2023-02-05 15:03:48 +0000

    added AVR toolchain

---
+++ cmake/gcc-avr.toolchain.cmake
@@ -0,0 +1,304 @@
+# toolchain prefix
+set(TRIPLE "avr")
+
+# attempt to find avr-gcc
+find_path(TOOLCHAIN_ROOT
+	NAMES
+		${TRIPLE}-gcc
+        ${TRIPLE}-g++
+	PATHS
+        # /usr/bin
+        # /usr/local/bin
+        # /bin
+        $ENV{AVR_ROOT}
+)
+
+# Error, could not find toolchain
+if(NOT TOOLCHAIN_ROOT)
+	message(FATAL_ERROR "Toolchain root could not be found!!!")
+endif(NOT TOOLCHAIN_ROOT)
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR avr)
+set(CMAKE_CROSS_COMPILING 1)
+
+set(CMAKE_C_COMPILER   "${TOOLCHAIN_ROOT}/${TRIPLE}-gcc${OS_SUFFIX}"     CACHE PATH "gcc"     FORCE)
+set(CMAKE_CXX_COMPILER "${TOOLCHAIN_ROOT}/${TRIPLE}-g++${OS_SUFFIX}"     CACHE PATH "g++"     FORCE)
+set(CMAKE_AR           "${TOOLCHAIN_ROOT}/${TRIPLE}-ar${OS_SUFFIX}"      CACHE PATH "ar"      FORCE)
+set(CMAKE_LINKER       "${TOOLCHAIN_ROOT}/${TRIPLE}-ld${OS_SUFFIX}"      CACHE PATH "linker"  FORCE)
+set(CMAKE_NM           "${TOOLCHAIN_ROOT}/${TRIPLE}-nm${OS_SUFFIX}"      CACHE PATH "nm"      FORCE)
+set(CMAKE_OBJCOPY      "${TOOLCHAIN_ROOT}/${TRIPLE}-objcopy${OS_SUFFIX}" CACHE PATH "objcopy" FORCE)
+set(CMAKE_OBJDUMP      "${TOOLCHAIN_ROOT}/${TRIPLE}-objdump${OS_SUFFIX}" CACHE PATH "objdump" FORCE)
+set(CMAKE_STRIP        "${TOOLCHAIN_ROOT}/${TRIPLE}-strip${OS_SUFFIX}"   CACHE PATH "strip"   FORCE)
+set(CMAKE_RANLIB       "${TOOLCHAIN_ROOT}/${TRIPLE}-ranlib${OS_SUFFIX}"  CACHE PATH "ranlib"  FORCE)
+set(AVR_SIZE           "${TOOLCHAIN_ROOT}/${TRIPLE}-size${OS_SUFFIX}"    CACHE PATH "size"    FORCE)
+
+set(AVR_LINKER_LIBS "-lc -lm -lgcc")
+
+if(NOT AVR_UPLOADTOOL)
+    set(
+            AVR_UPLOADTOOL avrdude
+            CACHE STRING "Set default upload tool: avrdude"
+    )
+    find_program(AVR_UPLOADTOOL avrdude)
+endif(NOT AVR_UPLOADTOOL)
+
+if(NOT AVR_SIZE_ARGS)
+    set(AVR_SIZE_ARGS -C;--mcu=${AVR_MCU})
+endif(NOT AVR_SIZE_ARGS)
+
+# default upload tool port
+if(NOT AVR_UPLOADTOOL_PORT)
+    set(
+            AVR_UPLOADTOOL_PORT usb
+            CACHE STRING "Set default upload tool port: usb"
+    )
+endif(NOT AVR_UPLOADTOOL_PORT)
+
+# default programmer (hardware)
+if(NOT AVR_PROGRAMMER)
+    set(
+            AVR_PROGRAMMER avrispmkII
+            CACHE STRING "Set default programmer hardware model: avrispmkII"
+    )
+endif(NOT AVR_PROGRAMMER)
+
+# use AVR_UPLOADTOOL_BAUDRATE as baudrate for upload tool (if defined)
+if(AVR_UPLOADTOOL_BAUDRATE)
+    set(AVR_UPLOADTOOL_BASE_OPTIONS ${AVR_UPLOADTOOL_BASE_OPTIONS} -b ${AVR_UPLOADTOOL_BAUDRATE})
+endif()
+
+##########################################################################
+# check build types:
+# - Debug
+# - Release
+# - RelWithDebInfo
+#
+# Release is chosen, because of some optimized functions in the
+# AVR toolchain, e.g. _delay_ms().
+##########################################################################
+if(NOT ((CMAKE_BUILD_TYPE MATCHES Release) OR
+(CMAKE_BUILD_TYPE MATCHES RelWithDebInfo) OR
+(CMAKE_BUILD_TYPE MATCHES Debug) OR
+(CMAKE_BUILD_TYPE MATCHES MinSizeRel)))
+    set(
+            CMAKE_BUILD_TYPE Release
+            CACHE STRING "Choose cmake build type: Debug Release RelWithDebInfo MinSizeRel"
+            FORCE
+    )
+endif(NOT ((CMAKE_BUILD_TYPE MATCHES Release) OR
+(CMAKE_BUILD_TYPE MATCHES RelWithDebInfo) OR
+(CMAKE_BUILD_TYPE MATCHES Debug) OR
+(CMAKE_BUILD_TYPE MATCHES MinSizeRel)))
+
+
+function(add_avr_executable EXECUTABLE_NAME)
+
+	# define file names we will be using
+	set(elf_file ${EXECUTABLE_NAME}_${AVR_MCU}.elf)
+	set(map_file ${EXECUTABLE_NAME}_${AVR_MCU}.map)
+	set(hex_file ${EXECUTABLE_NAME}_${AVR_MCU}.hex)
+	set(lst_file ${EXECUTABLE_NAME}_${AVR_MCU}.lst)
+    set(eeprom_image ${EXECUTABLE_NAME}_${AVR_MCU}-eeprom.hex)
+
+    
+	# add elf target
+	add_executable(${elf_file} ${ARGN})
+
+    # set compile and link flags for elf target
+    set_target_properties(
+        ${elf_file}
+        PROPERTIES
+            COMPILE_FLAGS "-mmcu=${AVR_MCU} -g -Os -w -std=gnu++11 -fno-exceptions -ffunction-sections -fdata-sections"
+            LINK_FLAGS    "-mmcu=${AVR_MCU} -Wl,-Map,${map_file} ${AVR_LINKER_LIBS}"
+    )
+
+     # create hex file
+     add_custom_command(
+        OUTPUT ${hex_file}
+        COMMAND
+            ${CMAKE_OBJCOPY} -j .text -j .data -O ihex ${elf_file} ${hex_file}
+        COMMAND
+            ${AVR_SIZE} ${AVR_SIZE_ARGS} ${elf_file}
+        DEPENDS ${elf_file}
+    )
+    
+    # generate the lst file
+    add_custom_command(
+    OUTPUT ${lst_file}
+    COMMAND
+        ${CMAKE_OBJDUMP} -h -S ${elf_file} > ${lst_file}
+    DEPENDS ${elf_file}
+    )
+
+    # eeprom
+    add_custom_command(
+    OUTPUT ${eeprom_image}
+    COMMAND
+       ${CMAKE_OBJCOPY} -j .eeprom --set-section-flags=.eeprom=alloc,load
+          --change-section-lma .eeprom=0 --no-change-warnings
+          -O ihex ${elf_file} ${eeprom_image}
+    DEPENDS ${elf_file}
+    )
+
+    # build the intel hex file for the device
+    add_custom_target(
+        ${EXECUTABLE_NAME}
+        ALL
+        DEPENDS ${hex_file} ${lst_file} ${eeprom_image}
+    )
+
+    set_target_properties(
+        ${EXECUTABLE_NAME}
+        PROPERTIES
+            OUTPUT_NAME ${elf_file}
+    )
+
+   # clean
+   get_directory_property(clean_files ADDITIONAL_MAKE_CLEAN_FILES)
+   set_directory_properties(
+      PROPERTIES
+         ADDITIONAL_MAKE_CLEAN_FILES "${map_file}"
+   )
+
+    # upload - with avrdude
+    add_custom_target(
+        upload_${EXECUTABLE_NAME}
+        ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} ${AVR_UPLOADTOOL_OPTIONS}
+           -U flash:w:${hex_file}
+           -P ${AVR_UPLOADTOOL_PORT}
+        DEPENDS ${hex_file}
+        COMMENT "Uploading ${hex_file} to ${AVR_MCU} using ${AVR_PROGRAMMER}"
+     )  
+
+   # disassemble
+   add_custom_target(
+      disassemble_${EXECUTABLE_NAME}
+      ${AVR_OBJDUMP} -h -S ${elf_file} > ${EXECUTABLE_NAME}.lst
+      DEPENDS ${elf_file}
+   )   
+
+endfunction(add_avr_executable)
+
+function(add_avr_library LIBRARY_NAME)
+    if(NOT ARGN)
+        message(FATAL_ERROR "No source files given for ${LIBRARY_NAME}.")
+    endif(NOT ARGN)
+
+    set(lib_file ${LIBRARY_NAME}${MCU_TYPE_FOR_FILENAME})
+    set (${LIBRARY_NAME}_LIB_TARGET ${elf_file} PARENT_SCOPE)
+
+    add_library(${lib_file} STATIC ${ARGN})
+
+    set_target_properties(
+            ${lib_file}
+            PROPERTIES
+            COMPILE_FLAGS "-mmcu=${AVR_MCU}"
+            OUTPUT_NAME "${lib_file}"
+    )
+
+    if(NOT TARGET ${LIBRARY_NAME})
+        add_custom_target(
+                ${LIBRARY_NAME}
+                ALL
+                DEPENDS ${lib_file}
+        )
+
+        set_target_properties(
+                ${LIBRARY_NAME}
+                PROPERTIES
+                OUTPUT_NAME "${lib_file}"
+        )
+    endif(NOT TARGET ${LIBRARY_NAME})
+
+endfunction(add_avr_library)
+
+
+function(avr_target_link_libraries EXECUTABLE_TARGET)
+   if(NOT ARGN)
+      message(FATAL_ERROR "Nothing to link to ${EXECUTABLE_TARGET}.")
+   endif(NOT ARGN)
+
+   get_target_property(TARGET_LIST ${EXECUTABLE_TARGET} OUTPUT_NAME)
+
+   foreach(TGT ${ARGN})
+      if(TARGET ${TGT})
+         get_target_property(ARG_NAME ${TGT} OUTPUT_NAME)
+         list(APPEND NON_TARGET_LIST ${ARG_NAME})
+      else(TARGET ${TGT})
+         list(APPEND NON_TARGET_LIST ${TGT})
+      endif(TARGET ${TGT})
+   endforeach(TGT ${ARGN})
+
+   target_link_libraries(${TARGET_LIST} ${NON_TARGET_LIST})
+endfunction(avr_target_link_libraries EXECUTABLE_TARGET)
+
+
+function(avr_target_include_directories EXECUTABLE_TARGET)
+    if(NOT ARGN)
+        message(FATAL_ERROR "No include directories to add to ${EXECUTABLE_TARGET}.")
+    endif()
+
+    get_target_property(TARGET_LIST ${EXECUTABLE_TARGET} OUTPUT_NAME)
+    set(extra_args ${ARGN})
+
+    target_include_directories(${TARGET_LIST} ${extra_args})
+endfunction()
+
+
+function(avr_target_compile_definitions EXECUTABLE_TARGET)
+    if(NOT ARGN)
+        message(FATAL_ERROR "No compile definitions to add to ${EXECUTABLE_TARGET}.")
+    endif()
+
+    get_target_property(TARGET_LIST ${EXECUTABLE_TARGET} OUTPUT_NAME)
+    set(extra_args ${ARGN})
+
+   target_compile_definitions(${TARGET_LIST} ${extra_args})
+endfunction()
+
+function(avr_generate_fixed_targets)
+   # get status
+   add_custom_target(
+      get_status
+      ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} -P ${AVR_UPLOADTOOL_PORT} -n -v
+      COMMENT "Get status from ${AVR_MCU}"
+   )
+   
+   # get fuses
+   add_custom_target(
+      get_fuses
+      ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} -P ${AVR_UPLOADTOOL_PORT} -n
+         -U lfuse:r:-:b
+         -U hfuse:r:-:b
+      COMMENT "Get fuses from ${AVR_MCU}"
+   )
+   
+   # set fuses
+   add_custom_target(
+      set_fuses
+      ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} -P ${AVR_UPLOADTOOL_PORT}
+         -U lfuse:w:${AVR_L_FUSE}:m
+         -U hfuse:w:${AVR_H_FUSE}:m
+         COMMENT "Setup: High Fuse: ${AVR_H_FUSE} Low Fuse: ${AVR_L_FUSE}"
+   )
+   
+   # get oscillator calibration
+   add_custom_target(
+      get_calibration
+         ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} -P ${AVR_UPLOADTOOL_PORT}
+         -U calibration:r:${AVR_MCU}_calib.tmp:r
+         COMMENT "Write calibration status of internal oscillator to ${AVR_MCU}_calib.tmp."
+   )
+   
+   # set oscillator calibration
+   add_custom_target(
+      set_calibration
+      ${AVR_UPLOADTOOL} ${AVR_UPLOADTOOL_BASE_OPTIONS} -P ${AVR_UPLOADTOOL_PORT}
+         -U calibration:w:${AVR_MCU}_calib.hex
+         COMMENT "Program calibration status of internal oscillator from ${AVR_MCU}_calib.hex."
+   )
+endfunction()
+
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
