bbe2e96b4d425bb4f0d1ba2a2206be119924e4e5
patrik <patrik.wall.91@gmail.com>
2023-02-11 20:15:59 +0000

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,19 @@
+#include <avr/interrupt.h>
 #include <avr/io.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <avr/interrupt.h>
-#include <stdbool.h>
 #include <util/delay.h>
 
-#include"Isomething.h"
+#include "Isomething.h"
 
 int main(void)
 {
-
-	while(1) {
-		PORTB |= (1<<PB0); //LED ON
-		_delay_ms(1000);
-		PORTB &= ~(1<<PB0); //LED OFF
-		_delay_ms(1000);
-		uint8_t asd = 6;
-	}
-
+   while (1) {
+      PORTB |= (1 << PB0); // LED ON
+      _delay_ms(1000);
+      PORTB &= ~(1 << PB0); // LED OFF ok
+      _delay_ms(1000);
+      uint8_t asd = 6;
+   }
 }
