3b3b1d42e97ad910dc2b30ef29864bbf773dc557
Azur <patrik.wall.91@gmail.com>
2023-02-18 13:55:21 +0000

    update the template

--- src/Isomething.h
+++ src/Isomething.h
@@ -1,12 +1,12 @@
 #ifndef SRC_ISOMETHING_H
 #define SRC_ISOMETHING_H
 
-#include<stdint.h>
+#include <stdint.h>
 
 class ISomething {
-    public:
-        virtual ~ISomething() {}
-        virtual uint8_t getInt() = 0;
+ public:
+   // virtual ~ISomething() {}
+   virtual uint8_t getInt() = 0;
 };
 
 #endif
