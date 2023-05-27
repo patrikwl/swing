8706319522e6a7bfbc783ce1bf51531651a07915
Your Name <patrik.wall.91@gmail.com>
2023-05-27 13:35:58 +0000

    add SPI interface

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <util/delay.h>
+#include "RegisterAccessor.h"
+#include "ISpiAdapter.h"
+#include "SpiAdapter.h"
+#include "SpiHandler.h"
 
-#include <something.h>
 
 extern "C" {
-#include "../include/lcd.h"
+// #include "../include/lcd.h"
 #include <string.h>
 }
 
@@ -22,14 +25,16 @@ ISR(INT1_vect)
 
 int main(void)
 {
-   Something something;
-   lcd_init(LCD_DISP_ON);
+   RegisterAccessor registerAccessor;
+   SpiAdapter spiAdapter{&registerAccessor};
+   ISpiAdapter::SpiConfig spiConf(ISpiAdapter::SpiFrequency::FOSC_8, PINB2, PINB3, PINB4, PINB5);
+   spiAdapter.configure(spiConf);
+   SpiHandler spiHandler{&spiAdapter};
    sei();
    while (1) {
       PORTB |= (1 << PB0); // LED ON
       _delay_ms(1000);
       PORTB &= ~(1 << PB0); // LED OFF ok
       _delay_ms(1000);
-      uint8_t asd = something.getInt();
    }
 }
