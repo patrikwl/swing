e95bb6b6105d4293d32996766ad41a09e6384362
Your Name <patrik.wall.91@gmail.com>
2023-06-04 20:58:57 +0000

    some clean-up

--- src/SpiHandler.cpp
+++ src/SpiHandler.cpp
@@ -1,10 +1,6 @@
 #include "SpiHandler.h"
 #include "ISpiAdapter.h"
 
-SpiHandler::SpiHandler(ISpiAdapter *theSpiAdapter)
-    : spiAdapter(theSpiAdapter) {}
-
-;
-// SpiHandler::~SpiHandler() {}
+SpiHandler::SpiHandler(ISpiAdapter *theSpiAdapter) : spiAdapter(theSpiAdapter) {}
 
 uint8_t const SpiHandler::transfer(uint8_t data) { return spiAdapter->transfer(data); }
